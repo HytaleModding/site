c1528f39d4a718d389a37abe759529c98008bea3
Neil <neil@willofsteel.me>
2026-06-05 17:35:15 +0530

    feat: update official hytale world gen v2 technical generator docs
    Signed-off-by: Neil <neil@willofsteel.me>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./styles/global.css";
 import "./styles/transitions.css";
 import "./styles/official-docs.css";
+import 'katex/dist/katex.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return children;
